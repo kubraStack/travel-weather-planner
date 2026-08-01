26328d6b92ea40e7264fe59be39edbd2dd5cd6df
Kubra Cakir <kmrt3014@gmail.com>
2026-08-02 00:04:19 +0300

    Inital commit: Add conditionals

---
+++ main.py
@@ -0,0 +1,25 @@
+distance_mi = 5
+is_raining = True
+has_bike = False
+has_car = True
+has_ride_share_app = True
+
+if not distance_mi:
+    print(False)
+elif distance_mi <= 1:
+    if not is_raining:
+        print(True)
+    else:
+        print(False)
+elif 1 < distance_mi <= 6:
+    if  is_raining and has_bike == False:
+        print(False)
+    elif not is_raining and not has_bike:
+        print(False)
+    else:
+        print(True)
+elif distance_mi > 6:
+    if has_car or has_ride_share_app:
+        print(True)
+    else:
+        print(False)
